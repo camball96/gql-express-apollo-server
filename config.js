c54d234aaed1c4a5056fb7e3c7b40e9dfe138986
Cam <cam.ball96@hotmail.com>
2021-08-17 16:19:20 +1000

    Added a config.js file to hold MONGODB info and added that file to gitignore to keep hidden. Also updated DB Key for security purposes

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB: 
+    'mongodb+srv://cam:12345@testing.pz1zl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -3,13 +3,14 @@ import express from 'express';
 import mongoose from 'mongoose'
 import { typeDefs } from './src/typeDefs'
 import { resolvers } from './src/resolvers'
+import { MONGODB } from './config'
 
 
 const startServer = async () => {
     const app = express();
     const server = new ApolloServer({typeDefs, resolvers});
     server.applyMiddleware({ app });
-    await mongoose.connect('mongodb+srv://cam:12345@testing.pz1zl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+    await mongoose.connect( MONGODB, {useNewUrlParser: true, useUnifiedTopology: true});
 
     app.listen({ port:4001 }, () =>
         console.log('Server ready'));
