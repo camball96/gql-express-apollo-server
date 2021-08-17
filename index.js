51802663d999ae1061af7bc937a9af8bd7016fc0
Cam <cam.ball96@hotmail.com>
2021-08-17 13:52:35 +1000

    Finished up typedefs and resolvers. Have added models folder with the newCar model. Connection to mongoAtlas is live and mutations working on playground. Server complete

--- index.js
+++ index.js
@@ -1,26 +1,18 @@
 import { ApolloServer, gql } from 'apollo-server-express';
 import express from 'express';
 import mongoose from 'mongoose'
+import { typeDefs } from './src/typeDefs'
+import { resolvers } from './src/resolvers'
 
-mongoose.connect('mongodb+srv://cam:12345@testing.pz1zl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 
-const app = express();
+const startServer = async () => {
+    const app = express();
+    const server = new ApolloServer({typeDefs, resolvers});
+    server.applyMiddleware({ app });
+    await mongoose.connect('mongodb+srv://cam:12345@testing.pz1zl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
 
-const typeDefs = gql`
-    type Query {
-        hello: String
-    }
-`;
-
-const resolvers = {
-    Query: {
-        hello: () => "Hello there"
-    }
+    app.listen({ port:4001 }, () =>
+        console.log('Server ready'));
 }
 
-const server = new ApolloServer({typeDefs, resolvers});
-
-server.applyMiddleware({ app });
-
-app.listen({ port:4000 }, () =>
-    console.log('Server ready'));
+startServer();
